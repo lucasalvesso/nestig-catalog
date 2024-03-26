7f20b49e64a8aa37314ce1a4719793cc1ad1d839
Lucas Oliveira <lcsalves@hotmail.com>
2024-03-26 10:09:06 -0300

    added frontend

--- backend/index.ts
+++ backend/index.ts
@@ -4,9 +4,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { routes } from "./src/routes";
-import {createConnection, getConnection} from "typeorm";
-import {container} from "tsyringe";
-import {Database} from "./src/repository/Database";
+import { createConnection, getConnection } from "typeorm";
+import { container } from "tsyringe";
+import { Database } from "./src/repository/Database";
 
 dotenv.config();
 
@@ -14,41 +14,41 @@ const app: express.Application = express();
 const port = 3000;
 
 app.use(
-    cors({
-        origin: ["http://localhost:4000"],
-        methods: ["GET", "POST", "OPTIONS"],
-        allowedHeaders: ["*"],
-    })
+  cors({
+    origin: ["http://localhost:4000"],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["*"],
+  }),
 );
 
 app.use(async (req, res, next) => {
-    let connection;
-    try {
-        connection = getConnection();
-    } catch (error) {
-        connection = await createConnection();
-    }
+  let connection;
+  try {
+    connection = getConnection();
+  } catch (error) {
+    connection = await createConnection();
+  }
 
-    container.register(Database, {
-        useValue: new Database(connection),
-    });
+  container.register(Database, {
+    useValue: new Database(connection),
+  });
 
-    next();
+  next();
 });
 
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  }),
 );
 
 routes(app);
 
 const main = () => {
-    app.listen(port, () => {
-        console.log("Server listening on port", port);
-    });
+  app.listen(port, () => {
+    console.log("Server listening on port", port);
+  });
 };
 
 main();
